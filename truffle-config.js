ac35292903e46d5d4cb0f2c9c37a79f8dd9ff869
saikishunsuke <saikishunsuke0828@yahoo.co.jp>
2019-11-22 15:50:32 +0900

    add vue app

--- truffle-config.js
+++ truffle-config.js
@@ -44,9 +44,14 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 7545,            // Standard Ethereum port (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*",
+    }
 
     // Another network with more advanced options...
     // advanced: {
